eb0e0c2780a059173f03a50583a98c0bf6005f5b
Jarret Lovelace <jarretlovelace@icloud.com>
2024-08-16 16:30:43 -0600

    WIP

--- api/server.js
+++ api/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const server = express();
 
-server.use(express.json());
+server.use(express.jso
 
+    server.use('*', (req, res) => {
+        res.status(404).json({
+            message: 'not found',
+        })
+    })
 module.exports = server;
